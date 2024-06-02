77f17f188c82896bb229c71c77001fccb9ecde07
Sreevishnu <sreeab28@gmail.com>
2024-06-03 00:02:32 +0530

    revert to recommended state till prisma support is fixed

--- fresh-todo-backend/db/client.ts
+++ fresh-todo-backend/db/client.ts
@@ -1,13 +1,12 @@
-import { PrismaClient } from "../generated/client/deno/edge.ts";
+import { PrismaClient } from "../generated/client/index.d.ts";
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-
 
 
 
 const env = await load();
 
-const prismaClient = new PrismaClient({
-  datasourceUrl: "postgresql://bot_user:password@localhost:5432/fresh_todo?schema=public"
+const prismaClient: PrismaClient = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL
 });
 
 export default prismaClient;
